1fe72885a503aa634561bb45851932cf86be5be9
Nishith-Savla <nishithsavla005@gmail.com>
2021-04-09 13:39:02 +0530

    Add centered reverse triangle number pattern program

---
+++ CenteredReverseTrianglePattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CenteredReverseTrianglePattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        short row, number, space, lines = Short.parseShort(scanner.nextLine());
+
+        for(row=lines; row > 0; --row) {
+            for(space=0; space < lines-row; ++space) {
+                System.out.print(' ');
+            }
+            for(number=1; number <= row; ++number) {
+                System.out.print(number + " ");
+            }
+            //for(space=0; space < lines-row; ++space) {
+                //System.out.println(' ');
+            //}
+            System.out.println();
+        }
+    }
+}
