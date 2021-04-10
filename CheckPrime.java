e3584c74bf63d591687e2bf2a9d144d5e8c40ba1
Nishith-Savla <nishithsavla005@gmail.com>
2021-04-10 23:30:21 +0530

    Create a prime number checker program

---
+++ CheckPrime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        if (num <= 0) {
+            System.out.println("Number should be positive & greater than zero");
+            System.exit(1);
+        } else if(num == 1) {
+            System.out.println("Neither prime nor composite" );
+            return;
+        }
+        boolean isNotPrime = false;
+        for (int i = 2; i < Math.sqrt(num); ++i) {
+            if(num % i == 0) {
+                System.out.println("NOT a Prime number (is divisible by " + i + ")");
+                isNotPrime = true;
+                break;
+            }
+        }
+        if(!isNotPrime) // If number is prime
+            System.out.println(num + " is a Prime number");
+    }
+}
